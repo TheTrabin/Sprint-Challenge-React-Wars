aef8d9ead8bd8661740dd1e73dbc8d7172df76a7
TheTrabin <beyondtears@gmail.com>
2020-04-19 20:56:22 -0700

    Managed MVP and answered the questions. Did not have enough time to work on any stretches.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
+import Axiom from './components/axios.js';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+ 
+  // const [people, setPeople] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+    <h1 className="Header">Rick and Morty Characters!</h1>  
+      
+      <Axiom />
+    
     </div>
   );
 }
